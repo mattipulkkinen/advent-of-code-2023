a09ec5e78ece865e0e3478a0fff545b6a20f0a4b
Matti Pulkkinen <133065141+mattipulkkinen@users.noreply.github.com>
2023-12-01 12:44:25 +0200

    Day 1 part 1 done

---
+++ src/main/java/com/github/mattipulkkinen/Main.java
@@ -0,0 +1,53 @@
+package com.github.mattipulkkinen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    List<Integer> calibrationValues = new ArrayList<>();
+    try (BufferedReader reader = Files.newBufferedReader(Path.of("input.txt"))) {
+      reader.lines().forEach(line -> {
+        char firstDigit = ' ';
+        char lastDigit = ' ';
+
+        // find the first digit
+        for (int i = 0; i < line.length(); i++) {
+          char current = line.charAt(i);
+          if (Character.isDigit(current)) {
+            firstDigit = current;
+            break;
+          }
+        }
+        // find the last digit
+        for (int i = line.length() - 1; i >= 0; i--) {
+          char current = line.charAt(i);
+          if (Character.isDigit(current)) {
+            lastDigit = current;
+            break;
+          }
+        }
+
+        if (firstDigit == ' ' || lastDigit == ' ') {
+          throw new IllegalStateException("failed to find both digits for line \"" + line + "\"");
+        }
+
+        int calibrationValue = Integer.parseInt(String.valueOf(firstDigit) + lastDigit);
+        calibrationValues.add(calibrationValue);
+      });
+    } catch (IOException e) {
+      System.err.println("Failed to open file for reading: " + e);
+      return;
+    }
+
+    int sum = calibrationValues.stream().reduce(0, Integer::sum);
+    System.out.println(sum);
+  }
+}
